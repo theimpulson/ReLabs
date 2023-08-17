e91e72c04b1206caf90eb4ce2354135658a932cb
Aayush Gupta <aayushgupta219@gmail.com>
2023-08-17 18:48:53 +0530

    Implement cache control
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/src/main/java/io/aayush/relabs/network/RetrofitModule.kt
+++ app/src/main/java/io/aayush/relabs/network/RetrofitModule.kt
@@ -1,16 +1,22 @@
 package io.aayush.relabs.network
 
+import android.content.Context
 import android.content.SharedPreferences
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import io.aayush.relabs.utils.CommonModule.ACCESS_TOKEN
+import okhttp3.Cache
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.io.File
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
+import okhttp3.CacheControl
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -31,18 +37,27 @@ class RetrofitModule {
     @Provides
     fun provideInterceptor(sharedPreferences: SharedPreferences): Interceptor {
         val accessToken = sharedPreferences.getString(ACCESS_TOKEN, "")
+        val cacheControl = CacheControl.Builder()
+            .maxAge(10, TimeUnit.DAYS)
+            .build()
+
         return Interceptor { chain ->
             val builder = chain.request().newBuilder()
-            builder.header("authorization", "Bearer $accessToken")
+                .header("Cache-Control", cacheControl.toString())
+                .header("authorization", "Bearer $accessToken")
             return@Interceptor chain.proceed(builder.build())
         }
     }
 
     @Singleton
     @Provides
-    fun provideOkHttpClient(interceptor: Interceptor): OkHttpClient {
+    fun provideOkHttpClient(
+        interceptor: Interceptor,
+        @ApplicationContext context: Context
+    ): OkHttpClient {
         return OkHttpClient.Builder()
             .addInterceptor(interceptor)
+            .cache(Cache(File(context.cacheDir, "http-cache"), 10L * 1024L * 1024L))
             .build()
     }
 }
