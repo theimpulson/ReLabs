e37ff0826c5db4cb36fcab49ea47b1d3d7f3af84
Aayush Gupta <aayushgupta219@gmail.com>
2023-08-19 19:15:45 +0530

    Theme: Set navigation bar color same as bottom navigation bar
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/src/main/java/io/aayush/relabs/ui/theme/Theme.kt
+++ app/src/main/java/io/aayush/relabs/ui/theme/Theme.kt
@@ -8,11 +8,13 @@ package io.aayush.relabs.ui.theme
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.platform.LocalContext
@@ -33,6 +35,15 @@ fun ReLabsTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable
     val systemUiController = rememberSystemUiController()
     LaunchedEffect(key1 = Unit) {
         systemUiController.setSystemBarsColor(colors.background, darkIcons = !darkTheme)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            val navigationBarElevation = NavigationBarDefaults.Elevation
+            systemUiController.setNavigationBarColor(
+                color = colors.surfaceColorAtElevation(
+                    navigationBarElevation
+                ), !darkTheme
+            )
+        }
     }
 
     MaterialTheme(colorScheme = colors, typography = Typography(), content = content)
