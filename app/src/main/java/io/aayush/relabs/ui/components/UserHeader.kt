5fce70ee5275e96760fafbc0850a258711de9084
Aayush Gupta <aayushgupta219@gmail.com>
2023-08-20 17:06:19 +0530

    Split out UserHeader information into separate file
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

---
+++ app/src/main/java/io/aayush/relabs/ui/components/UserHeader.kt
@@ -0,0 +1,112 @@
+package io.aayush.relabs.ui.components
+
+import android.os.Build
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.ImageLoader
+import coil.compose.SubcomposeAsyncImage
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.request.ImageRequest
+import coil.size.Scale
+import io.aayush.relabs.R
+import io.aayush.relabs.network.data.user.User
+import io.aayush.relabs.ui.theme.XDAYellow
+
+@Composable
+fun UserHeader(user: User) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+            .background(
+                color = MaterialTheme.colorScheme.secondaryContainer,
+                shape = RoundedCornerShape(20.dp)
+            ),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        SubcomposeAsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(user.profile_banner_urls.values.first()?.ifBlank { "" })
+                .crossfade(true)
+                .scale(Scale.FIT)
+                .build(),
+            contentDescription = "",
+            modifier = Modifier
+                .height(150.dp)
+                .padding(top = 10.dp, start = 10.dp, end = 10.dp)
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(20.dp)),
+            contentScale = ContentScale.Crop
+        )
+        Column(
+            modifier = Modifier.padding(top = 90.dp, bottom = 20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            SubcomposeAsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(user.avatar_urls.values.first()?.ifBlank { R.drawable.ic_account })
+                    .crossfade(true)
+                    .build(),
+                contentDescription = "",
+                imageLoader = ImageLoader.Builder(LocalContext.current)
+                    .components {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                            add(ImageDecoderDecoder.Factory())
+                        } else {
+                            add(GifDecoder.Factory())
+                        }
+                    }.build(),
+                modifier = Modifier
+                    .requiredSize(96.dp)
+                    .clip(CircleShape)
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = user.username, fontSize = 25.sp, fontWeight = FontWeight.Bold)
+                Spacer(modifier = Modifier.width(5.dp))
+                if (user.is_staff) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_mod),
+                        contentDescription = "",
+                        colorFilter = ColorFilter.tint(XDAYellow)
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(5.dp))
+            Text(
+                text = user.user_title,
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Medium,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
--- app/src/main/java/io/aayush/relabs/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/io/aayush/relabs/ui/screens/settings/SettingsScreen.kt
@@ -3,27 +3,15 @@ package io.aayush.relabs.ui.screens.settings
 import android.net.Uri
 import android.util.Log
 import androidx.browser.customtabs.CustomTabsIntent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredSize
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LinearProgressIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -31,23 +19,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
-import coil.compose.SubcomposeAsyncImage
-import coil.request.ImageRequest
 import io.aayush.relabs.R
 import io.aayush.relabs.network.data.user.Me
-import io.aayush.relabs.ui.theme.XDAYellow
+import io.aayush.relabs.ui.components.UserHeader
 
 private const val TAG = "SettingsScreen"
 
@@ -86,67 +66,16 @@ fun SettingsScreen(
             )
         }
     ) {
-        user.me.apply {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(it),
-                verticalArrangement = if (username.isBlank()) Arrangement.Center else Arrangement.Top,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                if (username.isNotBlank()) {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(10.dp)
-                            .background(
-                                color = MaterialTheme.colorScheme.secondaryContainer,
-                                shape = RoundedCornerShape(20.dp)
-                            )
-                    ) {
-                        Column(
-                            modifier = Modifier.padding(15.dp),
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
-                            SubcomposeAsyncImage(
-                                model = ImageRequest.Builder(LocalContext.current)
-                                    .data(avatar_urls.values.first())
-                                    .crossfade(true)
-                                    .build(),
-                                contentDescription = "",
-                                loading = {
-                                    CircularProgressIndicator()
-                                },
-                                modifier = Modifier
-                                    .requiredSize(96.dp)
-                                    .clip(CircleShape)
-                            )
-                            Spacer(modifier = Modifier.height(10.dp))
-                            Row(
-                                verticalAlignment = Alignment.CenterVertically
-                            ) {
-                                Text(text = username, fontSize = 25.sp, fontWeight = FontWeight.Bold)
-                                Spacer(modifier = Modifier.width(5.dp))
-                                if (is_staff) {
-                                    Image(
-                                        painter = painterResource(id = R.drawable.ic_mod),
-                                        contentDescription = "",
-                                        colorFilter = ColorFilter.tint(XDAYellow)
-                                    )
-                                }
-                            }
-                            Spacer(modifier = Modifier.height(5.dp))
-                            Text(
-                                text = user_title,
-                                fontSize = 15.sp,
-                                fontWeight = FontWeight.Medium,
-                                textAlign = TextAlign.Center
-                            )
-                        }
-                    }
-                } else {
-                    LinearProgressIndicator()
-                }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(it),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            if (user.me.username.isNotBlank()) {
+                UserHeader(user = user.me)
             }
         }
     }
