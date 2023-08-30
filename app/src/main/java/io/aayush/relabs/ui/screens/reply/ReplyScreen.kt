94e886aefe53b529c9c981e3911c90ca9caeb68b
Aayush Gupta <aayushgupta219@gmail.com>
2023-08-30 14:12:24 +0530

    PostReply: Remind people to read and follow the rules
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/src/main/java/io/aayush/relabs/ui/screens/reply/ReplyScreen.kt
+++ app/src/main/java/io/aayush/relabs/ui/screens/reply/ReplyScreen.kt
@@ -1,11 +1,14 @@
 package io.aayush.relabs.ui.screens.reply
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
@@ -13,6 +16,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -23,6 +27,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -30,8 +35,10 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
+import io.aayush.relabs.R
 import io.aayush.relabs.network.data.post.PostReply
 import io.aayush.relabs.ui.navigation.Screen
+import io.aayush.relabs.ui.theme.XDAYellow
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
@@ -92,17 +99,27 @@ fun ReplyScreen(
         Column(
             modifier = Modifier
                 .padding(it)
-                .fillMaxSize()
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(10.dp, Alignment.Top)
         ) {
-
+            OutlinedButton(
+                onClick = { navHostController.navigate(Screen.Thread.withID(4200559)) },
+                shape = RoundedCornerShape(10.dp),
+                border = BorderStroke(1.dp, XDAYellow),
+                modifier = Modifier.padding(horizontal = 10.dp)
+            ) {
+                Text(text = stringResource(id = R.string.follow_rules))
+            }
             OutlinedTextField(
                 modifier = Modifier
                     .fillMaxWidth()
                     .fillMaxHeight(0.5f)
-                    .padding(20.dp),
+                    .padding(horizontal = 15.dp),
                 value = text,
                 onValueChange = { newText -> text = newText },
-                enabled = !posting
+                enabled = !posting,
+                shape = RoundedCornerShape(10.dp),
             )
         }
     }
