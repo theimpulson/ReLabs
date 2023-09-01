61039ed009233cd5c270a316dd7dd9f098e423fb
Aayush Gupta <aayushgupta219@gmail.com>
2023-09-01 17:43:06 +0530

    Bump targetSdk to 34
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
     defaultConfig {
         applicationId = "io.aayush.relabs"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
