aedb955255088a51e0d12343dfdcbd9f2f23e0e5
Aayush Gupta <aayushgupta219@gmail.com>
2023-09-01 17:41:10 +0530

    Bump navigation compose to 2.7.1
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,7 +92,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // Navigation
-    implementation("androidx.navigation:navigation-compose:2.7.0")
+    implementation("androidx.navigation:navigation-compose:2.7.1")
 
     // OpenID
     implementation("net.openid:appauth:0.11.1")
