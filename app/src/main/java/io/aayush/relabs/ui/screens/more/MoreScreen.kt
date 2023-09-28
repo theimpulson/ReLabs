14e86f4f25215d93e11c73a714db662e17bf2b01
Aayush Gupta <aayushgupta219@gmail.com>
2023-09-28 17:59:51 +0530

    MoreScreen: Rework more screen
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/src/main/java/io/aayush/relabs/ui/screens/more/MoreScreen.kt
+++ app/src/main/java/io/aayush/relabs/ui/screens/more/MoreScreen.kt
@@ -1,33 +1,25 @@
 package io.aayush.relabs.ui.screens.more
 
-import android.net.Uri
-import android.util.Log
-import androidx.browser.customtabs.CustomTabsIntent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import io.aayush.relabs.R
-import io.aayush.relabs.network.data.user.Me
-import io.aayush.relabs.ui.components.UserHeader
 
 private const val TAG = "SettingsScreen"
 
@@ -37,35 +29,7 @@ fun MoreScreen(
     navHostController: NavHostController,
     viewModel: MoreViewModel = hiltViewModel()
 ) {
-    val user: Me? by viewModel.currentUser.collectAsStateWithLifecycle()
-
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        topBar = {
-            TopAppBar(
-                title = { Text(text = stringResource(id = R.string.more)) },
-                actions = {
-                    if (user?.me?.view_url?.isNotBlank() == true) {
-                        val context = LocalContext.current
-                        IconButton(onClick = {
-                            try {
-                                CustomTabsIntent.Builder()
-                                    .build()
-                                    .launchUrl(context, Uri.parse(viewModel.currentUser.value?.me?.view_url))
-                            } catch (exception: Exception) {
-                                Log.e(TAG, "Failed to open profile", exception)
-                            }
-                        }) {
-                            Icon(
-                                painter = painterResource(id = R.drawable.ic_open),
-                                contentDescription = ""
-                            )
-                        }
-                    }
-                }
-            )
-        }
-    ) {
+    Scaffold(modifier = Modifier.fillMaxSize()) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -74,9 +38,12 @@ fun MoreScreen(
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            if (user?.me?.username?.isNotBlank() == true) {
-                UserHeader(user = user?.me!!)
-            }
+            Image(
+                modifier = Modifier.requiredSize(196.dp),
+                painter = painterResource(id = R.drawable.ic_launcher_foreground_monochrome),
+                contentDescription = ""
+            )
+            Divider()
         }
     }
 }
