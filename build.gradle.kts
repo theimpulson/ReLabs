745a9b8577caa99e58f15e949ad9241344db9de8
Aayush Gupta <aayushgupta219@gmail.com>
2023-10-09 00:21:23 +0530

    Bump to latest stable AGP and Gradle versions
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.1" apply false
+    id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.5.1" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
