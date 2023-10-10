2f448a3d5b3e6bbf72ebfd0dd1e6710b43911823
Aayush Gupta <aayushgupta219@gmail.com>
2023-10-10 14:50:01 +0530

    [REVERT ME] MainActivity: Hide Forums tab for now
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/src/main/java/io/aayush/relabs/MainActivity.kt
+++ app/src/main/java/io/aayush/relabs/MainActivity.kt
@@ -38,7 +38,7 @@ class MainActivity : AppCompatActivity() {
         val topRoutes: List<Screen> = listOf(
             Screen.ThreadPreview,
             Screen.News,
-            Screen.ForumPreview,
+//            Screen.ForumPreview,
             Screen.Alerts,
             Screen.More
         )
