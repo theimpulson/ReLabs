22f5741848708c9700dd197cc5731db54d19dc8e
Aayush Gupta <aayushgupta219@gmail.com>
2023-10-20 18:58:36 +0530

    MoreScreen: Fix icon tint in dark theme
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/src/main/java/io/aayush/relabs/ui/screens/more/MoreScreen.kt
+++ app/src/main/java/io/aayush/relabs/ui/screens/more/MoreScreen.kt
@@ -1,6 +1,7 @@
 package io.aayush.relabs.ui.screens.more
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,9 +13,10 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -41,7 +43,8 @@ fun MoreScreen(
             Image(
                 modifier = Modifier.requiredSize(196.dp),
                 painter = painterResource(id = R.drawable.ic_launcher_foreground_monochrome),
-                contentDescription = ""
+                contentDescription = "",
+                colorFilter = ColorFilter.tint(if (isSystemInDarkTheme()) Color.White else Color.Black)
             )
             Divider()
         }
