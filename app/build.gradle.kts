325009eb04c8fbcadaa8478d05419af77b9436c3
Aayush Gupta <aayushgupta219@gmail.com>
2023-11-05 12:52:19 +0530

    0.0.3
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "io.aayush.relabs"
         minSdk = 21
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.0.2"
+        versionCode = 4
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
