044344cca64bb3ada927af4f80aac7b39705431e
Aayush Gupta <aayushgupta219@gmail.com>
2023-12-04 21:27:16 +0530

    Bump AGP, Gradle & dependencies to latest release
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,7 +85,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-util")
@@ -121,14 +121,14 @@ dependencies {
     implementation("com.prof18.rssparser:rssparser:6.0.4")
 
     // Browser (Custom Tab)
-    implementation("androidx.browser:browser:1.6.0")
+    implementation("androidx.browser:browser:1.7.0")
 
     // Material
     implementation("androidx.compose.material:material:1.5.4")
     implementation("com.google.android.material:material:1.10.0")
 
     // Hilt
-    val hiltVersion = "2.48.1"
+    val hiltVersion = "2.49"
     ksp("com.google.dagger:hilt-android-compiler:$hiltVersion")
     implementation("com.google.dagger:hilt-android:$hiltVersion")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
@@ -137,7 +137,7 @@ dependencies {
     val retrofitVersion = "2.9.0"
     implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
     implementation("com.squareup.retrofit2:converter-moshi:$retrofitVersion")
-    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
     // Coroutines
     val coroutinesVersion = "1.7.3"
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,10 @@
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.3" apply false
+    id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.6.1" apply false
-    id("com.google.dagger.hilt.android") version "2.48.1" apply false
+    id("com.google.dagger.hilt.android") version "2.49" apply false
     id("com.google.relay") version "0.3.09" apply false
 }
