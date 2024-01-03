0a333cefc9524d9bfadffbd143db85062de6f8dc
Aayush Gupta <aayushgupta219@gmail.com>
2024-01-03 21:51:26 +0530

    Bump dependencies to latest stable release
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.4"
+        kotlinCompilerExtensionVersion = "1.5.7"
     }
     packaging {
         resources {
@@ -85,7 +85,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-util")
@@ -101,7 +101,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // Navigation
-    implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation("androidx.navigation:navigation-compose:2.7.6")
 
     // OpenID
     implementation("net.openid:appauth:0.11.1")
@@ -118,17 +118,17 @@ dependencies {
     implementation("io.coil-kt:coil-gif:$coilVersion")
 
     // RSS-Parser
-    implementation("com.prof18.rssparser:rssparser:6.0.4")
+    implementation("com.prof18.rssparser:rssparser:6.0.6")
 
     // Browser (Custom Tab)
     implementation("androidx.browser:browser:1.7.0")
 
     // Material
     implementation("androidx.compose.material:material:1.5.4")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
 
     // Hilt
-    val hiltVersion = "2.49"
+    val hiltVersion = "2.50"
     ksp("com.google.dagger:hilt-android-compiler:$hiltVersion")
     implementation("com.google.dagger:hilt-android:$hiltVersion")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
