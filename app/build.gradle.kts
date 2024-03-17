6017375a2042f5bc56e78594c952a1814c242c1b
Aayush Gupta <aayushgupta219@gmail.com>
2024-03-17 08:55:52 +0530

    Bump AGP & dependencies to latest stable release
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,7 +85,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2024.02.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.02.02"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-util")
     implementation("androidx.compose.ui:ui-graphics")
@@ -94,7 +94,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.02"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
@@ -103,10 +103,10 @@ dependencies {
     implementation("androidx.security:security-crypto-ktx:1.1.0-alpha06")
 
     // Firebase
-    implementation("com.google.firebase:firebase-messaging-ktx:23.4.0")
+    implementation("com.google.firebase:firebase-messaging-ktx:23.4.1")
 
     // Navigation
-    implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 
     // OpenID
     implementation("net.openid:appauth:0.11.1")
@@ -118,7 +118,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
 
     // Coil
-    val coilVersion = "2.5.0"
+    val coilVersion = "2.6.0"
     implementation("io.coil-kt:coil:$coilVersion")
     implementation("io.coil-kt:coil-compose:$coilVersion")
     implementation("io.coil-kt:coil-gif:$coilVersion")
@@ -127,17 +127,18 @@ dependencies {
     implementation("com.prof18.rssparser:rssparser:6.0.6")
 
     // Browser (Custom Tab)
-    implementation("androidx.browser:browser:1.7.0")
+    implementation("androidx.browser:browser:1.8.0")
 
     // Material
-    implementation("androidx.compose.material:material:1.6.1")
+    implementation("androidx.compose.material:material:1.6.3")
     implementation("com.google.android.material:material:1.11.0")
 
     // Hilt
-    val hiltVersion = "2.50"
+    val hiltVersion = "2.51"
     ksp("com.google.dagger:hilt-android-compiler:$hiltVersion")
     implementation("com.google.dagger:hilt-android:$hiltVersion")
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     // Retrofit
     val retrofitVersion = "2.9.0"
@@ -146,7 +147,7 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
     // Coroutines
-    val coroutinesVersion = "1.7.3"
+    val coroutinesVersion = "1.8.0"
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
 }
