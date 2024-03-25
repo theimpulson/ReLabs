8444d54782a2ef69134f364e2b9a6d1ff1a75fc3
Aayush Gupta <aayushgupta219@gmail.com>
2024-03-25 14:10:17 +0530

    PostItem: Limit user title to one line
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/src/main/java/io/aayush/relabs/ui/components/PostItem.kt
+++ app/src/main/java/io/aayush/relabs/ui/components/PostItem.kt
@@ -34,6 +34,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -140,7 +141,9 @@ fun PostItem(
                         Text(
                             text = post.user.title,
                             fontSize = 13.sp,
-                            color = post.user.userTitle.color
+                            color = post.user.userTitle.color,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
                         )
                     }
                     Text(
