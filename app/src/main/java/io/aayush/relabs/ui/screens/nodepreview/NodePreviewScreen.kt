494788f201be243a6c4c1a0eda6d898a44fa3a94
Aayush Gupta <aayushgupta219@gmail.com>
2024-03-26 16:34:08 +0530

    NodePreviewScreen: Add support for searching forums
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/src/main/java/io/aayush/relabs/ui/screens/nodepreview/NodePreviewScreen.kt
+++ app/src/main/java/io/aayush/relabs/ui/screens/nodepreview/NodePreviewScreen.kt
@@ -1,15 +1,25 @@
 package io.aayush.relabs.ui.screens.nodepreview
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
@@ -18,6 +28,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -25,24 +36,81 @@ import androidx.compose.ui.util.fastForEachIndexed
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
+import androidx.paging.LoadState
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.itemContentType
+import androidx.paging.compose.itemKey
 import io.aayush.relabs.R
 import io.aayush.relabs.network.data.node.Kind
 import io.aayush.relabs.network.data.node.Node
-import io.aayush.relabs.ui.components.MainTopAppBar
+import io.aayush.relabs.network.data.thread.Thread
 import io.aayush.relabs.ui.components.NodePreviewItem
 import io.aayush.relabs.ui.navigation.Screen
 import kotlinx.coroutines.launch
 
 @Composable
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 fun NodePreviewScreen(
     navHostController: NavHostController,
     viewModel: NodePreviewViewModel = hiltViewModel()
 ) {
+
+    val searchResults = viewModel.searchResults.collectAsLazyPagingItems()
+    val shouldShowSearchResults by viewModel.shouldShowSearchResults.collectAsStateWithLifecycle()
+    val isSearching by viewModel.isSearching.collectAsStateWithLifecycle()
+    val searchText by viewModel.searchQuery.collectAsStateWithLifecycle()
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
-            MainTopAppBar(screen = Screen.NodePreview, navHostController = navHostController)
+            Box(modifier = Modifier.fillMaxWidth()) {
+                SearchBar(
+                    modifier = Modifier.align(Alignment.Center),
+                    query = searchText,
+                    onQueryChange = viewModel::updateQuery,
+                    onSearch = { viewModel.search(searchText) },
+                    active = isSearching,
+                    onActiveChange = {
+                        viewModel.isSearching.value = it
+                        if (!it) viewModel.updateQuery("")
+                    },
+                    placeholder = {
+                        Text(text = stringResource(id = R.string.search_hint_forum))
+                    },
+                    leadingIcon = {
+                        if (isSearching) {
+                            IconButton(
+                                onClick = {
+                                    viewModel.isSearching.value = false
+                                    viewModel.updateQuery("")
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                    contentDescription = ""
+                                )
+                            }
+                        } else {
+                            Icon(imageVector = Icons.Default.Search, contentDescription = "")
+                        }
+                    },
+                    trailingIcon = {
+                        if (isSearching) {
+                            IconButton(
+                                onClick = { viewModel.updateQuery("") },
+                                enabled = searchText.isNotBlank()
+                            ) {
+                                Icon(imageVector = Icons.Default.Close, contentDescription = "")
+                            }
+                        }
+                    }
+                ) {
+                    if (shouldShowSearchResults) {
+                        NodeItems(nodes = searchResults, navHostController = navHostController)
+                    }
+                }
+            }
         }
     ) {
         val tabData = listOf(R.string.inventory, R.string.watched)
@@ -122,6 +190,50 @@ fun NodePreviewScreen(
                             }
                         )
                     }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun NodeItems(nodes: LazyPagingItems<Node>, navHostController: NavHostController) {
+    LazyColumn(modifier = Modifier.fillMaxHeight()) {
+        when (nodes.loadState.refresh) {
+            is LoadState.Error -> {}
+            is LoadState.Loading -> {
+                items(20) {
+                    NodePreviewItem(modifier = Modifier.padding(10.dp), loading = true)
+                }
+            }
+
+            else -> {
+                items(
+                    count = nodes.itemCount,
+                    key = nodes.itemKey { t -> t.id },
+                    contentType = nodes.itemContentType { Thread::class.java },
+                ) { index ->
+                    val node = nodes[index] ?: return@items
+                    if (node.kind != Kind.FORUM) return@items
+
+                    NodePreviewItem(
+                        modifier = Modifier.padding(10.dp),
+                        title = node.title,
+                        company = node.breadcrumb_data.values.firstOrNull()?.title ?: String(),
+                        lastUpdated = node.node_type_data.firstOrNull()?.last_post_date?.long ?: 0,
+                        lastThreadTitle = node.node_type_data.firstOrNull()?.last_thread_title ?: "",
+                        unread = node.node_type_data.firstOrNull()?.isUnread ?: false,
+                        threads = node.node_type_data.firstOrNull()?.discussion_count ?: 0,
+                        iconURL = node.xda.iconUrl ?: "",
+                        onClicked = {
+                            navHostController.navigate(
+                                Screen.Node.withIDAndTitle(
+                                    node.id,
+                                    node.title
+                                )
+                            )
+                        }
+                    )
                 }
             }
         }
--- app/src/main/java/io/aayush/relabs/ui/screens/nodepreview/NodePreviewViewModel.kt
+++ app/src/main/java/io/aayush/relabs/ui/screens/nodepreview/NodePreviewViewModel.kt
@@ -2,6 +2,8 @@ package io.aayush.relabs.ui.screens.nodepreview
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.aayush.relabs.network.XDARepository
 import io.aayush.relabs.network.data.node.Node
@@ -9,12 +11,24 @@ import javax.inject.Inject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
 
 @HiltViewModel
 class NodePreviewViewModel @Inject constructor(
     private val xdaRepository: XDARepository
 ) : ViewModel() {
+
+    private val _searchResults = MutableStateFlow<PagingData<Node>>(PagingData.empty())
+    val searchResults = _searchResults.asStateFlow()
+
+    private val _searchQuery = MutableStateFlow("")
+    val searchQuery = _searchQuery.asStateFlow()
+
+    private val _shouldShowSearchResults = MutableStateFlow(false)
+    val shouldShowSearchResults = _shouldShowSearchResults.asStateFlow()
+
+    val isSearching = MutableStateFlow(false)
 
     private val _loading = MutableStateFlow(false)
     val loading = _loading.asStateFlow()
@@ -36,6 +50,25 @@ class NodePreviewViewModel @Inject constructor(
         if (!watchedNodes.value.isNullOrEmpty()) return
         viewModelScope.launch(Dispatchers.IO) {
             fetch { _watchedNodes.value = xdaRepository.getWatchedNodes() }
+        }
+    }
+
+    fun updateQuery(query: String) {
+        _searchQuery.value = query
+        if (query.isBlank()) _shouldShowSearchResults.value = false
+    }
+
+    fun search(query: String) {
+        _searchQuery.value = query
+        _shouldShowSearchResults.value = query.isNotBlank()
+
+        viewModelScope.launch {
+            xdaRepository.getSearchResultsForNodes(query)
+                .distinctUntilChanged()
+                .cachedIn(viewModelScope)
+                .collect {
+                    _searchResults.value = it
+                }
         }
     }
 
